e06db9a9b3bb9c275fd78f41b33248cb05f73386
jhonas <jhonas.burlamaqui@gmail.com>
2025-01-18 11:09:27 +0000

    XX

--- main.py
+++ main.py
@@ -3,48 +3,52 @@ from tabulate import tabulate
 from datetime import date
 import sys
 
-city = "Porto"
-country = "PT"
-day = "all"
+def main():
+	city = "Porto"
+	country = "PT"
+	day = "all"
+	
+	try:
+		for i in range(len(sys.argv))[1:]:
+			arg1, arg2 = sys.argv[i].split("=")
+			match arg1:
+				case "city":
+					city = arg2
+				case "country":
+					country = arg2
+				case "day":
+					day = arg2
+		
+		today = date.isoformat(date.today())
+		w = weather.Weather(city=city, country=country)
+		if day == "all":
+			t = tabulate(w.weather, headers="keys", tablefmt="fancy_grid")
+			print(t)
+			sys.exit()
+		elif day == "today":
+			table = []
+			for i in w.weather:
+				if i["date"] == today:
+					table.append(i)
+			t = tabulate(table, headers="keys", tablefmt="fancy_grid")
+			print(t)
+			sys.exit()
+		else:
+			table = []
+			for i in w.weather:
+				tday = i["date"].lstrip("2025-0")
+				if i["date"] == day or tday == day:
+					table.append(i)
+			t = tabulate(table, headers="keys", tablefmt="fancy_grid")
+			print(t)
+			sys.exit()
+		
+		print("no data")
+	except ValueError:
+		print(
+			"Usage: 'python3 main.py day=day city=city country=county'\n" +
+			"        day=yyyy-mm-dd or day=m-dd\n\n" +
+			"By defaut, eg: 'python3 main.py', city=Porto, country=PT, and day will show the following 5 days from today.")
 
-try:
-	for i in range(len(sys.argv))[1:]:
-		arg1, arg2 = sys.argv[i].split("=")
-		match arg1:
-			case "city":
-				city = arg2
-			case "country":
-				country = arg2
-			case "day":
-				day = arg2
-	
-	today = date.isoformat(date.today())
-	w = weather.Weather(city=city, country=country)
-	if day == "all":
-		t = tabulate(w.weather, headers="keys", tablefmt="fancy_grid")
-		print(t)
-		sys.exit()
-	elif day == "today":
-		table = []
-		for i in w.weather:
-			if i["date"] == today:
-				table.append(i)
-		t = tabulate(table, headers="keys", tablefmt="fancy_grid")
-		print(t)
-		sys.exit()
-	else:
-		table = []
-		for i in w.weather:
-			tday = i["date"].lstrip("2025-0")
-			if i["date"] == day or tday == day:
-				table.append(i)
-		t = tabulate(table, headers="keys", tablefmt="fancy_grid")
-		print(t)
-		sys.exit()
-	
-	print("no data")
-except ValueError:
-	print(
-		"Usage: 'python3 main.py day=day city=city country=county'\n" +
-		"        day=yyyy-mm-dd or day=m-dd\n\n" +
-		"By defaut, eg: 'python3 main.py', city=Porto, country=PT, and day will show the following 5 days from today.")
+if __name__ == "__main__":
+	main()
